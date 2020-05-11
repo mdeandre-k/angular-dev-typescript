719a9d2975c16aa154c73855a1aeb2acb6a1a835
Your Name <akhemraiev@luxoft.com>
2020-05-11 17:31:48 +0300

    - moves ui to AM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AuctionMaterialModule } from './auction-material.module';
@@ -30,7 +31,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     AuctionMaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  categories: string[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.categories = ['Phones', 'TVs', 'Laptops']
   }
 
 }
